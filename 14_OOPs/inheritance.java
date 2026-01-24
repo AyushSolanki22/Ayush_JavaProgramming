9373d9358614089f2bd1367aba574d2e8247a7de
AyushSolanki22 <0solanki.ayush@gmail.com>
2026-01-25 01:17:56 +0530

    Enhance comments on inheritance types in Java
    Added comments explaining types of inheritance in Java.

--- 14_OOPs/inheritance.java
+++ 14_OOPs/inheritance.java
@@ -1,3 +1,5 @@
+//Inheritance -- It is when properties and methods of base class(parent) are passed onto a derived class(child). Ex: Animal -- Fish
+//All derived classes get all properties and methods from base 
 public class inheritance {
     public static void main(String[] args) {
         Fish shark=new Fish();
@@ -34,3 +36,9 @@ class Fish extends Animal {
 
 //Four types of inheritance: single level, multi level, heirarchial inheritance, hybrid inheritance
 //there is also one type of imheritance too: multiple inheritance (though not directly possible/available in java, indirectly possible through interfaces)
+//multiple inheritance is basically a derived class geting properties and functions from two seperate base classes.
+
+//Single level --> Base class - Derived class 
+//Multi level --> Base class -- derived class -- derived class 
+//Hierarchical --> Base class -- derived 1, derived 2
+//Hybrid --> Combination of Multi level , Hierarchical 
