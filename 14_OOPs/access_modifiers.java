7d6bb760afcd244d35697ec3bc16721f7c185172
AyushSolanki22 <0solanki.ayush@gmail.com>
2026-01-24 22:08:28 +0530

    Enhance access_modifiers.java with comments on access modifiers
    Added comments explaining access modifiers in Java and their purpose.

--- 14_OOPs/access_modifiers.java
+++ 14_OOPs/access_modifiers.java
@@ -1,6 +1,13 @@
+//In Java, access modifiers (also known as access specifiers) are keywords that set the control of accessibility (visibility) of classes, interfaces, constructors, methods, and variables.  
+//It protect sensitive information and manage control of the data(information) where to be used/accessed or not, whether to be protected, private, and public.
+//They are a core part of the encapsulation principle in object-oriented programming, helping to control which parts of a program can access an element. 
+
+//Access Modifiers: Default,Private,Public, Protected
+
+//A package is group of classes, different classes or packages have full/limited/no access to each other like google maps doesn't need information/data of google pay.
 public class access_modifiers {
     public static void main(String[] args) {
-         BankAccount myAcc= new BankAccount();
+        BankAccount myAcc= new BankAccount();
         myAcc.username="ayushsolanki";
         // System.out.println(myAcc.password); //would cause error as no access
         myAcc.setPassword("ayush2208@@");
@@ -13,6 +20,7 @@ public class access_modifiers {
 class BankAccount{
     //allow user to set password , but don't allow to access/check password. 
     public String username;
+    
     private String password;
     public void setPassword(String pwd) {
         password=pwd;
