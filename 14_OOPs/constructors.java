bd3fc6f5071cf4c50230480719cf7f9be24efb79
AyushSolanki22 <0solanki.ayush@gmail.com>
2026-01-24 23:59:41 +0530

    Enhance comments on constructors in constructors.java
    Added comments explaining constructors and constructor overloading in Java.

--- 14_OOPs/constructors.java
+++ 14_OOPs/constructors.java
@@ -1,3 +1,10 @@
+// Constructor is a special method which is invoked automatically at the time of object creation. It is used to initialise newly created object
+// -Constructures have the same name as class or structure. Ex: Pen()
+// -Constructors don't have a return type (not even void)
+// -Constructors are only called once, at object creation.
+// -Memory allocation happens when constructor is called 
+
+
 public class constructors {
     public static void main(String[] args) {
         //constructor overloading -- when a programme do assign & work on passing given value itself to different constructors based on having difference in values automatically.--- polymorphism
@@ -11,6 +18,9 @@ class Student {
     String name;
     int roll; 
     String password;
+
+    //By default, when no constructor is created/defined, java creates a default constructor. - which is non-parameterised
+    //To create constructors along with having something done(printing something or passing values to variables in class ), it is necessary to define one with parameters.
 
     Student() {    // -- non-parameterized constructor
         System.out.println("constructor is called");
