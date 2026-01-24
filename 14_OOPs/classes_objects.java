c25df6f7109e249eae8407bb84dd3bf8c31634e1
AyushSolanki22 <0solanki.ayush@gmail.com>
2026-01-24 21:43:06 +0530

    update in classes_objects.java
    Updated comments for clarity and corrected explanations about object creation and memory allocation.

--- 14_OOPs/classes_objects.java
+++ 14_OOPs/classes_objects.java
@@ -1,9 +1,20 @@
+//#Objects
+//objects --> identities in the real world 
+//for example : animal, watch, pen
+
+//#Classes
+//Classes --> group of these identies (objects) - one class is a group of objects basically with similar properties snd functions (for eg: school class)
+//Class is eventually a blueprint of objects (for example : blueprint of M800: class, cars being created : objects 
+//classes has their attributes(properties) (ex: color) , and even functions(behaviours)(ex: changeColor()) for their objects to set properties and use functions according to need.
+
 public class classes_objects {
     public static void main(String[] args) {
-        //Pen ko actual banane ke liye public class mai constructor ke through  (P)
-        Pen p1=new Pen();//created a pen object called p1  //creating space in memory to store all properties of the class
+        //Pen ka actual mai as object banane ke liye public class mai constructor ke through create krrte hain (like we did for array, string )
+        Pen p1=new Pen();//created a pen object called p1 using constructor  //by using new keyword, we create class object's space in memory(heap, not in stack) to store all properties of the class in as ohject's properties 
+        //There are two types of memory : stack and neap --> objects are created in heap
         //Even if we haven't created a constructor, java would automatically create it.
         //But, by default, java creates constructor only when there is no other one.
+        
         p1.setColor("Blue");
         System.out.println(p1.color);
         p1.setTip(10);
@@ -11,7 +22,10 @@ public class classes_objects {
     }
 }
 public class Pen {
-    //properties+functions
+    //properties+functions. - are defined within classes 
+
+    //color,tip : properties,,,,,, setColor,setTip: functions 
+    
     String color="red";
     int tip;
 
@@ -21,4 +35,14 @@ public class Pen {
     void setTip(int newTip) {
         tip=newTip; 
     }
+}
+
+class Student{
+    String name;
+    int age ;
+    float percentage;
+
+    void calcPercentage(int phy, int chem, int math) {
+        percentage=(phy+chem+math)/3
+            }
 }
