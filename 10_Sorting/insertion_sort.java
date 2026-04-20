638957a3f6b8ef39b5e429e51a9d289436b1015a
Ayush <0solanki.ayush@gmail.com>
2026-04-21 00:32:37 +0530

    files added,updated

--- 10_Sorting/insertion_sort.java
+++ 10_Sorting/insertion_sort.java
@@ -1,28 +1,52 @@
 
 
 public class insertion_sort {
-    public static void insertionSort(int arr[]) {
-        int n=arr.length;
-        for (int i=1; i<=n-1; ++i) {
-            int cur=i;
-            for (int prev=i-1; prev>=0; --prev) {
-                if (arr[prev]>arr[cur] ) {
-                    int temp=arr[cur];
-                    arr[cur]=arr[prev];
-                    arr[prev]=temp;
-                }
-                --cur;       //it is because as the current element gets swapped to i-1 th index and also  need to be compared with adjacent previous element
-          }
-        }
+    // public static void insertionSort(int arr[]) {
+    //     int n=arr.length;
+    //     for (int i=1; i<=n-1; ++i) {
+    //         int cur=i;
+    //         for (int prev=i-1; prev>=0; --prev) {
+    //             if (arr[prev]>arr[cur] ) {
+    //                 int temp=arr[cur];
+    //                 arr[cur]=arr[prev];
+    //                 arr[prev]=temp;
+    //             }
+    //             --cur;       //it is because as the current element gets swapped to i-1 th index and also  need to be compared with adjacent previous element
+    //       }
+        // }
         // Arrays.sort(arr);
-        for (int i = 0; i <n; i++) {
-         System.out.println(arr[i]);   
-        }
+        // for (int i = 0; i <n; i++) {
+        //  System.out.println(arr[i]);   
+        // }
     
-    }
+    
     public static void main(String[] args) {
         int arr[]={5,4,1,3,2};
         
         insertionSort(arr);
+        for (int i = 0; i <n; i++) {
+        System.out.println(arr[i]);   
+        }
+    }
+
+    //correct(efficient insertion sort)
+    public static void insertionSort(int arr[]) {
+
+        int n = arr.length;
+
+        for(int i = 1; i < n; i++){
+            int cur=i;
+
+            // shift elements
+            while(cur > 0 && arr[cur-1] > arr[cur]){
+                //swap
+                int temp=arr[cur]; 
+                arr[cur]=arr[cur-1];
+                arr[cur-1]=temp;
+                cur--;
+            }
+
+            
+        }
     }
 }
--- 10_Sorting/selection_sort.java
+++ 10_Sorting/selection_sort.java
@@ -22,36 +22,62 @@ public class selection_sort {
 //         }
 
 //     }
-    public static void selsort(int num[]) {
-        int n=num.length;
-        for(int i=0; i<=n-2;++i){
-            int minPos=i;
-            for(int j=i+1; j<=n-1; ++j){
-                minPos=(num[j]<num[minPos])? j:minPos;
+//     public static void selsort(int num[]) {
+//         int n=num.length;
+//         for(int i=0; i<=n-2;++i){
+//             int minPos=i;
+//             for(int j=i+1; j<=n-1; ++j){
+//                 minPos=(num[j]<num[minPos])? j:minPos;
 
-        }
-        ///swap
-        int temp=num[i];
-        num[i]=num[minPos];
-        num[minPos]=temp;
+//         }
+//         ///swap
+//         int temp=num[i];
+//         num[i]=num[minPos];
+//         num[minPos]=temp;
         
-    }
-}
-    public static void main(String[] args) {
-        int num[]={5,4,1,3,2};
+//     }
+// }
+//     public static void main(String[] args) {
+//         int num[]={5,4,1,3,2};
 
-        selsort(num);
-
-
+//         selsort(num);
 
 
 
 
-        //output
-        for (int i=0; i<num.length; ++i) {
-            System.out.println(num[i]);
+
+
+//         //output
+//         for (int i=0; i<num.length; ++i) {
+//             System.out.println(num[i]);
+//         }
+//     }
+
+
+// }
+
+//best SELECTION SORT APPROACH (DSA)
+public static void main(String[] args){
+    int arr[]={5,-2,6,7,2,0,7,2};
+    int n=arr.length;
+    for(int i=0; i<n-1; ++i){             //same no. of passes as similar to that of bubble sort(n-1)passes 
+        int min=Integer.MAX_VALUE;
+        int minidx=-1;     //we also need minimum element index as well to have swapped elements(index is necessary for swapping)
+        for(int j=0; j<n;j++){
+            if(arr[j]<min){
+                min=arr[j];
+                minidx=min;
+            }
         }
-    }
+        //swap
+        int temp=min;
+        arr[minidx]=arr[i];
+        arr[i]=temp;
+    } 
+
+}
+
+
 
 
 }
